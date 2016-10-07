ded337fe1f5cc131155448d678dc093cd5752c03
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-07 11:33:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,100 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne=
+{
+    date:'October 7 2016',
+    heading: 'Article One',
+    title:'Article one',
+    content:`<p>
+                I just heard you found the one you've been looking for
+            </p>
+            <p>
+                Wish I knew that it wasn't me
+            </p>
+            <p>
+                Even after all this time I still wonder
+            </p>
+            <p>
+                Why we can't move on, just the way you did so easily
+            </p>
+            `
+};
 
+var articleTwo=
+{
+    date:'October 8 2016',
+    title:'Article two',
+    heading:'Article Two',
+    content:` 
+            <p>
+                Don't want to know
+            </p>
+            <p>
+                The kind of dress you're wearing tonight
+            </p>
+            <p>
+                If he's holding onto you so tight
+            </p>
+            <p>
+                The way I did before
+            </p> `
+};
+var articleThree=
+{
+    date:'October 9 2016',
+    title:'Article three',
+    heading:'Article Three',
+    content:`<p>
+                I overdosed
+            </p>
+            <p>
+                Should've known your love was a game
+            </p>
+            <p>
+                Can't get it out of my brain
+            </p>
+            <p>
+                Oh, it's just the same
+            </p> `
+};
+function createpage(data)
+{
+    var date=data.date;
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var htmltemplate=
+    `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <a href="/">Let's go back</a>
+            <hr>
+            <div>
+                <h2>
+                    ${heading}
+                </h2>
+            </div>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+    </html>
+    `
+    return htmltemplate;    
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,7 +110,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(create(articleOne));
+});
+app.get('/article-two', function (req, res) {
+  res.send(create(articleTwo));
+});
+app.get('/article-three', function (req, res) {
+  res.send(create(articleThree));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
