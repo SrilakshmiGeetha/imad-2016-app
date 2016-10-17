3bc9435c13e0f77d7d2193e42ff72184af05473a
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-17 21:01:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,9 +134,9 @@ app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createpage(articles[articleName]));
 });
-app.get('/Yes',function(req,res){
+app.get('/ui/main.js',function(req,res){
 
-    res.send(createpage(articleOne));
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
