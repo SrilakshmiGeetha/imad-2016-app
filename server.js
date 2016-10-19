c33dde05a90e760c335a9848c35f21ea964a9593
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 22:34:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,11 +121,19 @@ function createpage(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var count=0;
+app.get('/:Counter', function (req, res) {
+    count=count+1;
+    res.send(count.toString());
+  
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js',function(req,res){
 
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -138,16 +146,8 @@ app.get('/:articleName', function (req, res) {
     res.send(createpage(articles[articleName]));
   
 });*/
-var count=0;
-app.get('/:Counter', function (req, res) {
-    count=count+1;
-    res.send(count.toString());
-  
-});
-app.get('/ui/main.js',function(req,res){
 
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
