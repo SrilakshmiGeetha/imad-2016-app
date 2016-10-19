e3bcfa4014aa6267c8f45050a99a2c7154e4022f
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 22:54:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,12 +26,12 @@ button.onclick = function()
     var request=new XMLHttpRequest();
     request.onreadystatechange=function()
     {
-        if(request.readystate===XMLHttpRequest.Done)
+        if(request.readyState===XMLHttpRequest.Done)
         {
                 if(request.status==200)
                 {
                         var counter=request.responseText;
-                        var span=document.getElementById('times');
+                        var span=document.getElementById('count');
                         span.innerHTML=counter.toString();
                 }
         }
