81d5bbac55b12e21b818952ce497dc2602463123
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 19:35:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,14 +117,14 @@ function createpage(data)
     `
     return htmltemplate;    
 }
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/ui/main.js',function(req,res){
 
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
