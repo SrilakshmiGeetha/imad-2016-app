30ab6ea6760d4f4b0cb77b3a66a88785b3966725
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 19:20:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,10 @@ function createpage(data)
     `
     return htmltemplate;    
 }
+app.get('/ui/main.js',function(req,res){
+
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -137,10 +141,7 @@ app.get('/:articleName', function (req, res) {
     res.send(createpage(articles[articleName]));
   
 });*/
-app.get('/ui/main.js',function(req,res){
 
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
