49ec9b1d5c95b491791dc6014371bd2a1cb018b7
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 19:23:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ var button=document.getElementById('Press');
 button.onclick= function()
 {
     counter=counter+1;
+    alert("Hello");
     var span=documemnt.getElementById('times');
     span.innerHTML=counter.toString();
 };
