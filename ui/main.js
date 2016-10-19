2aa8957dea435cbf39b5cdc4dd2e23f9ca44358f
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 18:48:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
 console.log('Loaded!');
 var element=document.getElementById('image');
+var x=0;
+function moveRight()
+{
+        x=x+1;
+        element.style.marginLeft=x+'px';
+}
 
 element.onclick=function()
 {
     var interval=setInterval(moveRight,50);
 };
-function moveRight()
-{
-        marginLeft=marginLeft+1;
-        element.style.marginLeft=marginLeft+'px';
-}
