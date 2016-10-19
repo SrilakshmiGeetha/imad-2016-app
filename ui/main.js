ddd480617f166dead3dde6142662a1bab58b7061
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-19 22:25:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onclick= function()
     var span=documemnt.getElementById("times");
     span.innerHTML=counter.toString();
 };*/
- counter=0;
+ /*counter=0;
             var beaut=document.getElementById("yes");
             beaut.onclick= function()
             {
@@ -17,4 +17,24 @@ button.onclick= function()
                 var span=document.getElementById("times");
                 span.innerHTML=counter.toString();
                 
-            }; 
+            }; */
+var button=document.getElementById('counter');
+button.onclick=function()
+{
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readystate===XMLHttpRequest.Done)
+        {
+                if(request.status==200)
+                {
+                        var counter=request.responseText;
+                        var span=document.getElementById('times');
+                        span.innerHTML=counter.toString();
+                }
+        }
+    };
+    request.open('GET','http://srilakshmigeetha.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
