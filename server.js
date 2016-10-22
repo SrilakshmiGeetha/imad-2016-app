fd59078dac87c9dd84e5a57244637b69cb4428ca
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-22 16:42:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ var names=[];
 
 app.get('/submit-name',function(req,res)
 {
-    var names=req.query.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
     
