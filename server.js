67ae33d194b1760961771300c483b45dd4e6b113
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-22 16:53:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,6 +127,10 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
   
 });
+app.get('/ui/main.js',function(req,res){
+
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 var names=[];
 
 app.get('/submit-name',function(req,res)
@@ -139,10 +143,7 @@ app.get('/submit-name',function(req,res)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/main.js',function(req,res){
 
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
