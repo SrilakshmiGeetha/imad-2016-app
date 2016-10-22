918914ac66534db077ffb8a7c4ff0bf2297770a3
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-22 17:02:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ button.onclick = function()
             if(request.status===200)
             {
                 var names=request.responseText;
-                names=JSON.parse();
+                names=JSON.parse(names);
                 var list='';
                 for(var i=0;i<names.length;i++)
                 {
