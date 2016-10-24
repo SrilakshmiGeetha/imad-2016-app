eda07b0e081667ac95df38b6cec681f6b19a415d
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-24 23:55:09 +0530

    [imad-console] Updates scripting.js

--- scripting.js
+++ scripting.js
@@ -22,10 +22,11 @@ function moveRight()
     {
         Top=Top+ 20;
         Bottom=Bottom-20;
+         y.style.marginTop=Top+'px';
+        pic.style.marginTop=Bottom+ 'px';
         y.style.width='50px';
         y.style.height='50px';
-        y.style.marginTop=Top+'px';
-        pic.style.marginTop=Bottom+ 'px';
+       
        
     }
     document.addEventListener("keydown", function(event)
