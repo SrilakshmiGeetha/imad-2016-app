a4f3be3991a51613eeae24f1d2a6b40271429c6b
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-24 18:37:28 +0530

    [imad-console] Updates scripting.js

--- scripting.js
+++ scripting.js
@@ -1,2 +1,12 @@
 var x=document.getElementById("heli");
 x.style.marginLeft='1000px';
+var margin=1000;
+function moveRight()
+{
+    margin=margin-100;
+    x.style.marginLeft=margin+ 'px';
+}
+x.onclick=function()
+{
+    var interval=setInterval(moveRight,100); 
+};
