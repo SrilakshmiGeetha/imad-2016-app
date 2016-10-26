89718c1fdcdba41fba1234c7b47f616737dbd049
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-26 21:13:50 +0530

    [imad-console] Updates scripting.js

--- scripting.js
+++ scripting.js
@@ -31,7 +31,7 @@ function moveRight()
     {
         
         Top=Top+ 20;
-        Bottom=Bottom-15;
+        Bottom=Bottom-20;
         y.style.marginTop=Top+ 'px';
        pic.style.marginTop=Bottom+ 'px';
         y.style.width='50px';
