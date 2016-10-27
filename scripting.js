08e79dea259e4e9598aa9383b61bb56711def6db
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-10-27 20:55:03 +0530

    [imad-console] Updates scripting.js

--- scripting.js
+++ scripting.js
@@ -30,8 +30,8 @@ function moveRight()
    else if((z>=1)&&(Top<=500))
     {
         
-        Top=Top+ 20;
-        Bottom=Bottom-20;
+        Top=Top+ 30;
+        Bottom=Bottom-30;
         y.style.marginTop=Top+ 'px';
         pic.style.marginTop=Bottom+ 'px';
         y.style.width='50px';
