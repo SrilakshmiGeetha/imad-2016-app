d1dd63afda68d5d526628214215aca1b9f44e8d1
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-08 22:43:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var array='';
 app.get('/test',function(req,res)
 {
     console.log("connection made");
-    pool.query('SELECT * FROM personalities WHERE id=3',function(err,result)
+    pool.query('SELECT * FROM personalities WHERE ID=3',function(err,result)
     {
       if(err)
         {
