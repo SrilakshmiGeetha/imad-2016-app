5ccac0f00e664910b912453b77cb42fa4ded45e5
SrilakshmiGeetha <srilakshmigeetha36@gmail.com>
2016-11-08 15:38:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ function createpage(data)
 }
 app.get('/',function(req,res)
 {
-    res.sendFine(_dirname,'ui','index.html');
+    res.sendFine(path.join(_dirname,'ui','index.html'));
 });
 app.get('/favicon.ico',function(req,res)
 {
@@ -173,10 +173,18 @@ app.get('/submit-name/:name',function(req,res)
     
 });
 
+app.get('/ui/main.js',function(req,res){
+
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/style.css', function (req, res) 
+{
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
 
 
-
-app.get('/Layout.html',function(req,res)
+/*app.get('/Layout.html',function(req,res)
 {
     res.sendFine(_dirname,'Layout.html');
 });
@@ -188,14 +196,9 @@ app.get('/data.js',function(req,res){
 
     res.sendFile(path.join(__dirname, 'data.js'));
 });
-app.get('/ui/main.js',function(req,res){
 
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
+/*
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
