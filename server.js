8af3b5666840dfc09f1f16a142a0b6b1b73adeab
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-09 12:03:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,81 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+
 
 var app = express();
 app.use(morgan('combined'));
 
-var config=
-{
-    user: 'srilakshmigeetha',
-    database: 'srilakshmigeetha',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password: process.env.DB_PASSWORD
-};
-var pool= new Pool(config);
 
-
-function template(array)
-{
-   // res.send(JSON.stringify(array));
-    var id=array.id;
-    var name=array.Name;
-    var templates=
-    `
-    <html>
-    <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                ${id}
-            </div>
-            <div>
-                ${name}
-            </div>
-        </div>
-    </body>
-    </html>
-    `
-    return templates;    
-}
-  
-
-
-app.get('/test',function(req,res)
-{
-    console.log("connection made");
-    pool.query('SELECT * FROM personalities WHERE id<3',function(err,result)
-    {
-      if(err)
-        {
-            console.log("Hello");
-            res.status(500).send(err.toString());
-            
-        }
-        else
-        {
-            if(result.rows.length === 0)
-            {
-                res.status(404).send('Not found');
-            }
-            else
-            {
-                 var array=result.rows[0];
-                 res.send(template(array));
-            //     res.send(JSON.stringify(result[0]));
-             //   res.send(JSON.stringify(array));
-                
-                
-            }
-        }
-        
-    });
-});
     
 var articles=
 {
@@ -194,7 +125,10 @@ app.get('/',function(req,res)
 {
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
+app.get('/ui/Data.js',function(req,res){
 
+    res.sendFile(path.join(__dirname, 'ui', 'Data.js'));
+});
 app.get('/ui/favicon.ico',function(req,res)
 {
     res.sendFile(path.join(__dirname,'ui','favicon.ico'));
