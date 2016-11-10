1f4b616ea3ae6cf0d1481cb592c5f5024f8edfd8
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-10 11:32:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -195,8 +195,9 @@ app.post('/create-user',function(req,res)
    var username=req.params.username;
    var password=req.params.password;
    var salt=crypto.randomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbString],function(err,result)
-   {
+   {console.log("hello");
         if(err)
         {
             res.status(500).send(err.toString());
