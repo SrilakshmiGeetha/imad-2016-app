8164237e7fd0d54b9234b16505b79d12eea821fd
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-10 11:25:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto=require('crypto');
 var app = express();
 app.use(morgan('combined'));
 
@@ -179,11 +180,34 @@ function createpage(data)
     `
     return htmltemplate;    
 }
+function hash(input,salt)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
+    return ["pbkdf2","1000",salt,hashed.toString('hex')].join('$');
+}
 app.get('/',function(req,res)
 {
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
+app.get('/create-user',function(req,res)
+{
+   var username=req.params.username;
+   var password=req.params.password;
+   var salt=crypto.randomBytes(128).toString('hex');
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbString],function(err,result)
+   {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        
+        else
+        {
+            res.status(200).send("Succesfully created"+ username);
+        }
+   });
+});
 app.get('/ui/favicon.ico',function(req,res)
 {
     res.sendFile(path.join(__dirname,'ui','favicon.ico'));
