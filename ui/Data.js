0b9c4a44e22ceed89d7384920585ee2c54075651
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-12 12:33:21 +0530

    [imad-console] Updates ui/Data.js

--- ui/Data.js
+++ ui/Data.js
@@ -20,7 +20,7 @@ function dummy(){
     var username=document.getElementById('username1').value;
     var password=document.getElementById('password1').value;
     console.log(username);
-    request.open('POST','/login' ,true);
+    request.open('POST','http://srilakshmigeetha.imad.hasura-app.io/login' ,true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({"username": username, "password": password}));
 }
