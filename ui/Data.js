168c0283dde152e57ebb08e860ac46d7beca1761
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-12 12:18:36 +0530

    [imad-console] Updates ui/Data.js

--- ui/Data.js
+++ ui/Data.js
@@ -1,5 +1,27 @@
 function dummy(){
-    alert("dummy called");
+ var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            
+            if(request.status===200)
+            {
+                alert("Logging in");
+            }
+            else if(request.status===500)
+            {
+                alert("something went wrong :(");
+            }
+        }
+    };
+    var username=document.getElementById('username1').value;
+    var password=document.getElementById('password1').value;
+    console.log(username);
+    request.open('POST','http://srilakshmigeetha.imad.hasura-app.io/login' ,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 }
 /*
 var x=document.getElementById("submitbutton");
@@ -62,29 +84,7 @@ var y=document.getElementById("button1");
  y.onclick=function()
  {
      alert("processing");
-   var request=new XMLHttpRequest();
-    request.onreadystatechange=function()
-    {
-        
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            
-            if(request.status===200)
-            {
-                alert("Logging in");
-            }
-            else if(request.status===500)
-            {
-                alert("something went wrong :(");
-            }
-        }
-    };
-    var username=document.getElementById('username1').value;
-    var password=document.getElementById('password1').value;
-    console.log(username);
-    request.open('POST','http://srilakshmigeetha.imad.hasura-app.io/login' ,true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({"username": username, "password": password}));
+   
      
 };
    */
