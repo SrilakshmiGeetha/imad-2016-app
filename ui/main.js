6181f8e8d961bfba78a2bbd5d9dfb272620cd2ff
Srilakshmi Geetha <srilakshmigeetha36@gmail.com>
2016-11-12 20:21:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,6 +56,32 @@ button.onclick = function()
 
 
 */
+function dummy(){
+ var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            
+            if(request.status===200)
+            {
+                console.log("LOGGING IN");
+                alert("Logging in");
+            }
+            else if(request.status===500)
+            {
+                alert("something went wrong :(");
+            }
+        }
+    };
+    var username=document.getElementById('username1').value;
+    var password=document.getElementById('password1').value;
+    console.log(username);
+    request.open('POST','http://srilakshmigeetha.imad.hasura-app.io/login' ,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({"username": username, "password": password}));
+}
 
  var x=document.getElementById("button");
  
